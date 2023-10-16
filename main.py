751bd18115aee07fbd80edea9185b99989d7be83
Briaccc <briac@fromager.net>
2023-10-15 22:56:46 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,202 @@
+import json
+import requests
+from msal import ConfidentialClientApplication
+import os
+import zipfile
+import email
+from email import policy
+from bs4 import BeautifulSoup
+import base64
+
+
+
+client_id = "APP_ID"
+client_secret = "SECRET"
+tenant_id = "TENANT_ID"
+
+msal_authority = f"https://login.microsoftonline.com/{tenant_id}"
+
+msal_scope = ["https://graph.microsoft.com/.default"]
+
+msal_app = ConfidentialClientApplication(
+    client_id=client_id,
+    client_credential=client_secret,
+    authority=msal_authority,
+)
+
+result = msal_app.acquire_token_silent(
+    scopes=msal_scope,
+    account=None,
+)
+
+if not result:
+    result = msal_app.acquire_token_for_client(scopes=msal_scope)
+
+if "access_token" in result:
+    access_token = result["access_token"]
+else:
+    raise Exception("No Access Token found")
+
+headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json",
+}
+
+# ----------------------------------------------------------------------------------------------------
+
+                
+#OPTION 1
+#OPTION 1
+#OPTION 1
+#OPTION 1
+
+
+# ----------------------------------------------------------------------------------------------------
+
+def get_user_profile_by_email(email):
+    # Construire l'URL pour obtenir le profil de l'utilisateur par courriel
+    user_profile_url = f"https://graph.microsoft.com/v1.0/users/{email}"
+
+    # Obtenez le jeton d'accès comme vous l'avez fait dans votre code initial
+
+    # Faites une requête GET pour obtenir les informations du profil
+    response = requests.get(
+        url=user_profile_url,
+        headers=headers,
+    )
+
+    if response.status_code == 200:
+        user_profile = response.json()
+        print("Informations du profil de l'utilisateur:")
+        print(json.dumps(user_profile, indent=4))
+    else:
+        print(f"Impossible d'obtenir les informations du profil de l'utilisateur. Code de statut : {response.status_code}")
+
+# ----------------------------------------------------------------------------------------------------
+
+                
+#OPTION 2
+#OPTION 2
+#OPTION 2
+#OPTION 2
+
+# ----------------------------------------------------------------------------------------------------
+
+
+def get_last_5_emails_by_user(email):
+    # Rechercher l'ID de l'utilisateur par courriel
+    user_search_url = f"https://graph.microsoft.com/v1.0/users?$filter=mail eq '{email}'"
+
+    # Obtenir le jeton d'accès comme dans votre code initial
+
+    # Faire une requête GET pour rechercher l'ID de l'utilisateur
+    response = requests.get(
+        url=user_search_url,
+        headers=headers,
+    )
+
+    if response.status_code == 200:
+        user_data = response.json()
+        if user_data.get("value"):
+            user_id = user_data["value"][0]["id"]
+            # Obtenir les 5 derniers courriels reçus de l'utilisateur
+            user_emails_url = f"https://graph.microsoft.com/v1.0/users/{user_id}/messages?$select=sender,subject,receivedDateTime,id&$top=5&$orderby=receivedDateTime desc"  # Ajout de 'id'
+
+            # Faire une requête GET pour obtenir les courriels
+            response = requests.get(
+                url=user_emails_url,
+                headers=headers,
+            )
+
+            if response.status_code == 200:
+                emails = response.json()
+                print("Les 5 derniers courriels reçus de l'utilisateur:")
+                for email in emails.get("value", []):
+                    # Assurez-vous que la structure de l'objet 'email' correspond à celle de la réponse JSON
+                    print(f"ID du courriel : {email.get('id', 'N/A')}")  # Ajout de l'ID
+                    print(f"Objet : {email.get('subject', 'N/A')}")
+                    print(f"Reçu le : {email.get('receivedDateTime', 'N/A')}\n")
+            else:
+                print(f"Impossible d'obtenir les courriels de l'utilisateur. Code de statut : {response.status_code}")
+        else:
+            print(f"Utilisateur avec l'adresse e-mail {email} non trouvé.")
+    else:
+        print(f"Impossible de rechercher l'utilisateur. Code de statut : {response.status_code}")
+
+
+# ----------------------------------------------------------------------------------------------------
+
+# OPTION 3
+# OPTION 3
+# OPTION 3
+# OPTION 3
+
+# ----------------------------------------------------------------------------------------------------
+
+# Fonction pour télécharger un message en format MIME
+def download_message_mime(recipient_email, message_id):
+    # Construisez l'URL pour obtenir le message
+    email_search_url = f"https://graph.microsoft.com/v1.0/users/{recipient_email}/messages/{message_id}/$value"
+    
+    # Faites une requête GET pour obtenir le message
+    response = requests.get(
+        url=email_search_url,
+        headers=headers,
+    )
+
+    if response.status_code == 200:
+        message_mime_content = response.content
+        
+        # Créez un message MIME à partir du contenu du message
+        message = email.message_from_bytes(message_mime_content, policy=policy.default)
+        
+        # Enregistrez le message dans un fichier texte nommé 'courriel.txt'
+        with open('courriel.txt', 'w', encoding='utf-8') as file:
+            file.write(message.as_string())
+            
+        print("Le message a été enregistré dans le fichier 'courriel.txt'.")
+
+        # Téléchargez les pièces jointes s'il y en a
+        for part in message.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue  # Évitez les parties multipart
+            if part.get('Content-Disposition') is None:
+                continue  # Ignorez les parties sans disposition de contenu
+
+            # Obtenez le nom de la pièce jointe
+            attachment_filename = part.get_filename()
+            
+            if attachment_filename:
+                # Enregistrez la pièce jointe localement
+                with open(attachment_filename, 'wb') as attachment_file:
+                    attachment_file.write(part.get_payload(decode=True))
+                print(f"Pièce jointe téléchargée : {attachment_filename}")
+    else:
+        print(f"Impossible d'obtenir les informations du message. Code de statut : {response.status_code}")
+
+
+# Menu
+
+while True:
+    print("Menu:")
+    print("0. Quitter")
+    print("1. Lire les informations d'un utilisateur")
+    print("2. Lire les 5 derniers courriels d'un utilisateur")
+    print("3. Téléchargé le courriel en format MIME et les attachements")
+
+    choice = input("Sélectionnez une option (0/1/2/3) : ")
+
+    if choice == '0':
+        break  # Quitter le programme
+    elif choice == '1':
+        email = input("Entrez le courriel de l'utilisateur : ")
+        get_user_profile_by_email(email)
+    elif choice == '2':
+        email = input("Entrez le courriel de l'utilisateur : ")
+        get_last_5_emails_by_user(email)
+    elif choice == '3':
+        recipient_email = input("Entrez l'adresse électronique du destinataire du message : ")
+        message_id = input("Entrez le ID du message : ")
+        download_message_mime(recipient_email, message_id)
+    else:
+        print("Option invalide. Sélectionnez 0, 1, 2, 3.")
